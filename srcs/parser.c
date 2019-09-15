3edeb0efc9c87ed84417800314f401bca4a555a1
Myriam BENZARTI <mybenzar@student.42.fr>
2019-09-15 11:43:42 +0200

    [LEAKS] still trying to fix leak from print_begin_file

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 10:49:32 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/09/08 19:36:59 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/09/15 11:42:50 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 #include <stdio.h>
 
 /*
-**	--> translate_char_list_to_array : converts the t_lstch list of chars into
-**	a list of strings t_lstst (contained in t_lemin structure)that we will be
-**	able to use to pursue parsing
-*/
+ **	--> translate_char_list_to_array : converts the t_lstch list of chars into
+ **	a list of strings t_lstst (contained in t_lemin structure)that we will be
+ **	able to use to pursue parsing
+ */
 
 static int	translate_char_list_to_array(t_lstch *list, t_lemin *data, int len)
 {
@@ -34,13 +34,13 @@ static int	translate_char_list_to_array(t_lstch *list, t_lemin *data, int len)
 		return (1);
 	node->line = data->file->line + 1;
 	node->str = (char*)malloc(sizeof(char) * (len + 1));
-	node->len = len;
-	node->next = NULL;
 	if (node->str == NULL)
 	{
 		free(node);
 		return (1);
 	}
+	node->len = len;
+	node->next = NULL;
 	while (list && i < len)
 	{
 		node->str[i++] = list->c;
@@ -53,13 +53,35 @@ static int	translate_char_list_to_array(t_lstch *list, t_lemin *data, int len)
 	return (0);
 }
 /*
-**	--> add_char_to_list_before_translation : saves each char from the file
-**	into a t_lstch list.
-**	This method was used to avoid doing multiple "strjoins".
+static int	translate_char_list_to_array(t_lstch *list, t_lemin *data, int len)
+{
+	int			i;
+	t_lstch		*tmp;
+
+	i = 0;
+	tmp = list;
+	if (!(data->file->next = (t_lstst*)malloc(sizeof(t_lstst))))
+		return (1);
+	data->file->next->line = data->file->line + 1;
+	data->file = data->file->next;
+	data->file->len = len;
+	if (!(data->file->str = (char*)malloc(sizeof(char) * (len + 1))))
+		return (1);
+	while (list && (data->file->str[i++] = list->c))
+		list = list->next;
+	data->file->next = NULL;
+	return (0);
+}
 */
 
+/*
+ **	--> add_char_to_list_before_translation : saves each char from the file
+ **	into a t_lstch list.
+ **	This method was used to avoid doing multiple "strjoins".
+ */
+
 static int	add_char_to_list_before_translation(t_lstch **actual
-	, char c, t_lstch **begin)
+		, char c, t_lstch **begin)
 {
 	if (!(*begin))
 	{
@@ -82,7 +104,7 @@ static int	add_char_to_list_before_translation(t_lstch **actual
 }
 
 static int	translate_line_and_verify_if_well_formated(t_lstch **actual
-	, t_lstch **begin, t_lemin *data, int *count)
+		, t_lstch **begin, t_lemin *data, int *count)
 {
 	int	ret;
 	static t_lstch *cur = NULL;
@@ -94,7 +116,7 @@ static int	translate_line_and_verify_if_well_formated(t_lstch **actual
 	if (add_char_to_list_before_translation(actual, 0, &cur))
 		ret = 1;
 	if (ret == 0 && translate_char_list_to_array(cur, data, *count))
-		 ret = 1;
+		ret = 1;
 	if (ret == 0 && check_if_line_can_probably_be_well_formated(cur, data, *count, i))
 		ret = 1;
 	*actual = cur;
@@ -104,16 +126,16 @@ static int	translate_line_and_verify_if_well_formated(t_lstch **actual
 }
 
 /*
-**	--> read_loop : reads through the file 1 byte at a time to make
-**	the reading process more secure. Every byte is added to a list
-**	of chars, and there's an extra security check with
-**	probably_be_well_formated in case the file is 4096 bites long.
-**	It calls if_well_formated once it has read a line or reached
-** 	EOF and then checks the whole line and translates it.
-*/
+ **	--> read_loop : reads through the file 1 byte at a time to make
+ **	the reading process more secure. Every byte is added to a list
+ **	of chars, and there's an extra security check with
+ **	probably_be_well_formated in case the file is 4096 bites long.
+ **	It calls if_well_formated once it has read a line or reached
+ ** 	EOF and then checks the whole line and translates it.
+ */
 
 static int	read_loop(t_lemin *data, t_lstch **begin, t_lstch **actual
-	, int ret[2])
+		, int ret[2])
 {
 	char	buf;
 
@@ -124,7 +146,7 @@ static int	read_loop(t_lemin *data, t_lstch **begin, t_lstch **actual
 			if (*actual == NULL && add_char_to_list_before_translation(actual, buf, begin))
 				return (free_char_list(begin, 1));
 			if (translate_line_and_verify_if_well_formated(actual, begin
-					, data, &ret[1]))
+						, data, &ret[1]))
 				return (free_char_list(begin, 1));
 		}
 		else
@@ -134,7 +156,7 @@ static int	read_loop(t_lemin *data, t_lstch **begin, t_lstch **actual
 			if (++(ret[1]) % 4096 == 0)
 			{
 				if (check_if_line_can_probably_be_well_formated(*begin, data
-						, ret[1], 1))
+							, ret[1], 1))
 					return (free_char_list(begin, 1));
 			}
 		}
@@ -143,9 +165,9 @@ static int	read_loop(t_lemin *data, t_lstch **begin, t_lstch **actual
 }
 
 /*
-**	--> parser : reads through the file, checks that it's correct, and saves it
-**	into lists of chars
-*/
+ **	--> parser : reads through the file, checks that it's correct, and saves it
+ **	into lists of chars
+ */
 
 int			parser(t_lemin *data)
 {
